8fb28b0c5e1bebb2526ab4faa05f610a4960b013
Marcin Chlebowski <sirowirdo@gmail.com>
2015-08-02 19:58:11 +0200

    Stworzenie obrazu planszy i siatki

--- GoApp/src/board/BoardView.java
+++ GoApp/src/board/BoardView.java
@@ -1,14 +1,58 @@
 package board;
 
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class BoardView extends JPanel{
 	private BoardControl boardController;
 	private BoardModel boardModel;
+	private BufferedImage board;
 	
 	public BoardView(BoardControl bc, BoardModel bm){
 		setSize(600, 600);
 		setLayout(null);
 		
+		try {
+		File f = new File("./resources/board.png");		
+			board = ImageIO.read(f);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.print(this.toString() + " b³¹d wczytywania t³a planszy");
+		}
+		
+		repaint();
+	}
+	
+	public void paintComponent(Graphics g){
+		g.drawImage(board, 0, 0, null);
+		
+		int lineLength= (int) (this.getHeight() - 50)/9;
+		
+		char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+		char[] numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+		
+		int linePointX = 50;
+		int linePointY = 50;
+		int lineEndPointX = linePointX + lineLength * 8;
+		int lineEndPointY = linePointY + lineLength * 8;
+		int letterStartX = 45;
+		int letterStartY = 45;
+		for(int i = 0; i < 9; i++){
+			g.drawLine(linePointX, linePointY, lineEndPointX, linePointY);
+			g.drawLine(linePointY, linePointX, linePointY, lineEndPointY);
+			
+			g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+			g.drawString(String.valueOf(letters[i]), letterStartX, letterStartY);
+			
+			letterStartX += lineLength;
+			linePointY += lineLength;
+		}
 	}
 }
--- GoApp/src/main/MainBoard.java
+++ GoApp/src/main/MainBoard.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
-import options.BottomPanelControl;
-import options.BottomPanelView;
 import options.OptionPanelControl;
 import options.OptionPanelView;
 import board.BoardView;
@@ -13,26 +11,21 @@ import board.BoardView;
 public class MainBoard extends JFrame{
 	private BoardView board;
 	private OptionPanelControl optionPanelControl;
-	private BottomPanelControl bottomPanelControl;
 	private OptionPanelView optionPanelView;
-	private BottomPanelView bottomPanelView;
 	
 	public MainBoard(BoardView board){
 		this.board = board;
 		
 		setSize(800, 600);
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	setResizable(false);
+    	setResizable(true);
     	
     	optionPanelControl = new OptionPanelControl();
-    	bottomPanelControl = new BottomPanelControl();
     	
     	optionPanelView = optionPanelControl.getOptionPanelView();
-    	bottomPanelView = bottomPanelControl.getBottomPanelView();
     	
     	add(BorderLayout.CENTER, board);
     	add(BorderLayout.EAST, optionPanelView);
-    	add(BorderLayout.SOUTH, bottomPanelView);
     	
     	setVisible(true);    	
     	repaint();
